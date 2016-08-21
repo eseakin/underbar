07d79856a50f0839e250a10f07260a5ac1f095d7
Eric Eakin <eseakin@gmail.com>
2016-08-21 16:08:56 -0700

    Assignment COMPLETE -- figured out _.filter and _.reject

--- src/underbar.js
+++ src/underbar.js
@@ -99,17 +99,32 @@
 
     };
 
-/*
-    for(var i = 0; i < collection.length; i++){
-      if (test(collection[i])) {
-        newArray.push((collection[i]));
+/* For Reference
+  _.filter = function(collection, test) {
+    var newArray = [];
+
+    _.each(collection,function(ele, i, array){
+      if (test(ele)) {
+        newArray.push(ele);
       }
-    }
+    });
     return newArray;
-  };
+
+    };
 */
 
   // Return all elements of an array that don't pass a truth test.
+  _.reject = function(collection, test) {
+    // TIP: see if you can re-use _.filter() here, without simply
+    // copying code in and modifying it
+
+    return _.filter(collection,function(num){
+      return !test(num)
+    });
+
+  };
+
+  /* Previous version
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
@@ -121,29 +136,6 @@
     });
     return newArray;
   };
-
-  /*
-      function antiTest (tester) {
-      return !tester;
-    }
-
-    _.filter(collection, antiTest(test));
-
-    var newArray = [];
-    return newArray.push(!_.filter(collection, test));
-
-
-  -----------------------
-      reject(collection, test)
-   var selected = [];
-
-  for (var i = 0; i < arr.length; i++) {
-    if (!cb(arr[i], i, arr)) {
-      selected.push(arr[i]);
-    }
-  }
-
-  return selected;
   */
 
   // Produce a duplicate-free version of the array.
